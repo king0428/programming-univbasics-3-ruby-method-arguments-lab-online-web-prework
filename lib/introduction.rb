b4d3a3c6f9a5aa8cc9f84eb7f5e8e652c1890534
Geovany Valdovinos <brownvdoe@gmail.com>
2019-09-16 20:46:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,7 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 
-def introduction(name = Josh)
+def introduction_with_language(Josh, )
   puts "Hi my name is #{name}."
 end
 
