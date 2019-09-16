9c75a1fff978105c907d513eed30df975450ea83
Geovany Valdovinos <brownvdoe@gmail.com>
2019-09-16 21:06:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,11 +3,12 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
-name=Josh
-introduction(name)
+
 def introduction(name)
   puts "HI my name is #{name}."
 end
+
+introducton("Josh")
 
 
 
