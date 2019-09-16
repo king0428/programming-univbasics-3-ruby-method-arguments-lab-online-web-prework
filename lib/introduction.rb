0c057b0ce7567a225dc880ad49462eb98ee202e9
Geovany Valdovinos <brownvdoe@gmail.com>
2019-09-16 21:24:47 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -18,7 +18,12 @@ end
 introduction_with_language("Josh", "React")
 
 
-def introduction_with_language_optional
+def introduction_with_language_optional(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language_optional("Josh", language="Ruby")
+
 
 
 
